91ab2a263a78cff69f4c1aa7512e0b4086305e66
OHTA Shogo <athos0220@gmail.com>
2015-02-04 21:25:02 +0900

    Add admin model

---
+++ src/checkin_halake/models/admin.clj
@@ -0,0 +1,21 @@
+(ns checkin-halake.models.admin
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [environ.core :refer [env]]
+            [checkin-halake.models.core :refer [db]]
+            [crypto.password.bcrypt :refer [encrypt check]]))
+
+(defn- fix-admin [admin]
+  (dissoc admin :password))
+
+(defn register-admin [email password name]
+  (let [password (encrypt password)
+        admin {:_id email, :password password, :name name}]
+    (-> (mc/insert-and-return db "admin" admin)
+        fix-admin)))
+
+(defn login [email password]
+  (assert (and (seq email) (seq password)))
+  (let [{crypted-password :password :as admin} (mc/find-one-as-map db "admin" {:_id email})]
+    (when (check password crypted-password)
+      (fix-admin admin))))
