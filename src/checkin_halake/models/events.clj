0440c6311fe6db8f7481a2d9743cfb23358222b9
OHTA Shogo <athos0220@gmail.com>
2015-02-04 18:11:23 +0900

    Stringify event IDs

--- src/checkin_halake/models/events.clj
+++ src/checkin_halake/models/events.clj
@@ -3,18 +3,20 @@
             [environ.core :refer [env]])
   (:use checkin-halake.models.core))
 
-(defn- fix-event [doc]
-  (dissoc doc :password))
+(defn- fix-event [event]
+  (-> event
+      (assoc :id (str (:_id event))) ;; FIXME: :id is a necessary field?
+      (dissoc :_id)))
 
 (defn register-event [title image-url event-at content-url]
   (let [event {:title title, :image-url image-url,
                :event-at event-at, :content-url content-url,
                :createdAt (java.util.Date.)}]
-    (mc/insert-and-return db "events" event)))
+    (fix-event (mc/insert-and-return db "events" event))))
 
 ;; (register-event "Swift開発講座" "/images/evnets/inside_halake.png" (java.util.Date ) "taro@email.com" "hoge" "Taro" "090")
 
 (defn query-events []
-  (mc/find-maps db "events"))
+  (map fix-event (mc/find-maps db "events")))
 
 ;; (query-events)
